e640f822b79a9c9584d891062b83cd89af7926d8
dineshkush <kushwahd18@gmail.com>
2023-05-20 14:09:32 +0530

    backend

--- frontend/src/components/chatpages/SingleChat.js
+++ frontend/src/components/chatpages/SingleChat.js
@@ -13,7 +13,7 @@ import io from "socket.io-client";
 import Lottie from "react-lottie";
 import animationData from "../animations/typing.json";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "http://localhost:5001";
 var socket, selectedChatCompare;
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
   const { user, selectedChat, setSelectedChat, notification, setNotification } =
